ddf22454b0b59afa3c10b117e4dde88ab9939746
devinlightfoot <euler2171828@gmail.com>
2020-08-01 00:12:16 -0700

    added authentication functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
     include ArticlesHelper
+    before_action :require_login, only: [:destroy, :create, :edit, :new, :update]
     def index
         @articles = Article.all
     end
